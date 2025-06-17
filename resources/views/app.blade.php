ad0f1130aa8ceb78a58299f1f8fcd19ad303e572
tomyates <twyates@proton.me>
2025-06-17 21:59:29 +0100

    File path routes updated.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -37,7 +37,10 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @routes
-        @vite(['resources/vendor/cms-core/js/app.ts'])
+        @vite([
+            'vendor/cms-core/resources/js/app.ts',
+            'vendor/cms-core/resources/css/app.css'
+        ])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
