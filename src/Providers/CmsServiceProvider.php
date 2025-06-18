94029f10c2164841c598cd9b7cc103de049a8066
tomyates <twyates@proton.me>
2025-06-18 12:06:52 +0100

    Published config files to make app use as default config.

--- src/Providers/CmsServiceProvider.php
+++ src/Providers/CmsServiceProvider.php
@@ -12,17 +12,29 @@ class CmsServiceProvider extends ServiceProvider
 
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
-        $this->loadViewsFrom(__DIR__.'/../../resources/views', 'cms');
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'cms');
 
         $this->publishes([
-            __DIR__.'/../../resources/js' => resource_path('vendor/cms-core/js'),
+            __DIR__ . '/../../resources/js' => resource_path('vendor/cms-core/js'),
         ], 'cms-assets');
+
+        $this->publishes([
+            __DIR__ . '/../config' => config_path(),
+        ], 'cms-config');
+    }
+
+    public function register()
+    {
+        foreach (glob(__DIR__ . '/../config/*.php') as $file) {
+            $key = basename($file, '.php'); 
+            $this->mergeConfigFrom($file, $key);
+        }
     }
 
     protected function registerRoutes()
     {
         \Route::middleware('web')
             ->namespace('BaseCms\Http\Controllers')
-            ->group(__DIR__.'/../../routes/web.php');
+            ->group(__DIR__ . '/../../routes/web.php');
     }
 }
